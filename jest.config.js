0fe6966fcb722ab1925c4a344e9eb907d7131a83
abhinavgupta11 <abhinav.gupta@orangeskill.com>
2026-05-26 11:48:40 +0530

    Changes

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(react-native|@react-native|@react-navigation|react-native-screens|react-native-safe-area-context|react-native-linear-gradient)/)',
   ],
+  // do not run end-to-end tests as part of unit test runs
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/e2e/'],
   moduleNameMapper: {
     '\\.(ttf|png|jpg|jpeg|gif|webp)$': '<rootDir>/__mocks__/fileMock.js',
   },
